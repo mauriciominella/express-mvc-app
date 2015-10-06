2ad677d8d29499884609bf6a4dc68d4186c6bab6
Mauricio Minella <mauriciominella@gmail.com>
2015-10-05 22:17:37 -0300

    bower-files gulp task created

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-// Gulpfile.js 
+// Gulpfile.js
 var gulp = require('gulp')
   , nodemon = require('gulp-nodemon')
- 
+  , gulpBowerFiles = require('gulp-bower-files');
+
 gulp.task('dev', function () {
   nodemon({ script: 'app.js'
           , ext: 'html js'
-          , ignore: ['ignored.js'] })
+          , ignore: ['ignored.js']
+          //, tasks: ['bower-files']
+        })
     .on('restart', function () {
       console.log('restarted!')
     })
 })
+
+
+ gulp.task('bower-files', function(){
+   //gulpBowerFiles().pipe(gulp.dest('./public'));
+   //console.log('public files generated!');
+ });
